2d4920fab1750c70399aa7b176aca0a07570cadd
Crowdbotics <team@crowdbotics.com>
2023-07-21 17:42:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "taste-42330",
+  url: "https://taste-42330.botics.co",
+  api: "https://taste-42330.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
